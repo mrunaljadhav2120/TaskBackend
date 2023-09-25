31c94047af30a35610c8cc0c28960bfed99cff28
Mrunal <mrunaljadhav2120@gmail.com>
2023-09-25 19:43:38 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+const port = 4000;
+const API = "mongodb+srv://jadhavmrunal2109:moL3sMgjljIDhtRX@cluster2.lxltldl.mongodb.net/Taskmanage?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', false);
+
+
+async function main() {
+    await mongoose.connect(API);
+    console.log('connected to database');
+    app.listen(port, () => console.log(`Server is live at PORT => ${port}`));
+};
+main();
